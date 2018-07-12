a3e218f61d85a8c5ddc45875bebec2ddb378d9e4
ryoma08 <ryoma08@vdesktop.7build.com>
2018-07-12 16:33:00 +0800

    Initial commit

---
+++ app/src/main/java/com/lgh/wine/api/ApiService.java
@@ -0,0 +1,16 @@
+package com.lgh.wine.api;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+/**
+ * Created by niujingtong on 2018/7/6.
+ * 模块：
+ */
+public interface ApiService {
+
+//    @GET("rkl/search")
+//    Call<RaoKouLingResult> getData(@Query("appkey") String appkey, @Query("keyword") String key);
+}
---
+++ app/src/main/java/com/lgh/wine/api/Constant.java
@@ -0,0 +1,10 @@
+package com.lgh.wine.api;
+
+/**
+ * Created by niujingtong on 2018/7/6.
+ * 模块：
+ */
+class Constant {
+
+    public static String BASE_IP = "http://api.jisuapi.com/";
+}
